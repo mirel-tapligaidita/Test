75e12c35dc72c9bc933da97b0424ffa6a92430b6
Mirel Tapliga-Idita <tapligaidita.mirel@gmail.com>
2014-10-26 19:43:15 +0200

    Primul commit

---
+++ src/com/base/Main.java
@@ -0,0 +1,16 @@
+package com.base;
+
+/**
+ * Created by Mirel on 26.10.2014.
+ *
+ * @author Mirel Tapliga-Idita
+ * @version 1.1
+ * @since 2014-10-26
+ */
+public class Main {
+
+    public static void Main(String[] args) {
+
+
+    }
+}
